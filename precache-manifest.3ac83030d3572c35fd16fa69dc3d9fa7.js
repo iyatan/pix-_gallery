f962cce62b52134c7e5ad7789da1f4fe184146e6
Iyatan Atchoro <iyatanatchoro@Iyatans-MacBook-Air.local>
2019-02-11 01:34:15 -0500

    Updates

---
+++ precache-manifest.3ac83030d3572c35fd16fa69dc3d9fa7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ada4eb1c545e3643c90e",
+    "url": "/pix_gallery/static/css/main.f730c629.chunk.css"
+  },
+  {
+    "revision": "ada4eb1c545e3643c90e",
+    "url": "/pix_gallery/static/js/main.ada4eb1c.chunk.js"
+  },
+  {
+    "revision": "19b1bc68ce5bb6c3f725",
+    "url": "/pix_gallery/static/js/1.19b1bc68.chunk.js"
+  },
+  {
+    "revision": "0f7e8681d6dfae0fe6e7",
+    "url": "/pix_gallery/static/js/runtime~main.0f7e8681.js"
+  },
+  {
+    "revision": "7a11c54da221704e0c0546ffcf81de2d",
+    "url": "/pix_gallery/index.html"
+  }
+];
